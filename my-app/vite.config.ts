32de809d46fe4db3a733d0f726d22e10953f31b0
gwizxs <156608329+gwizxs@users.noreply.github.com>
2024-05-25 22:30:34 +0300

    добавил корневые файлы в отдельную папку

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -5,10 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'index.html'
-    }
-  }
 })
